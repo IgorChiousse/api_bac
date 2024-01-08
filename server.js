c8288971c1cdc0f41813ad43d56614ca10fd8112
IgorChiousse <igor.chiousse@gmail.com>
2024-01-08 13:46:38 +0100

    mise en place et instalation du serveur

--- server.js
+++ server.js
@@ -15,10 +15,37 @@ const app = express();
 
 // Middleware pour traiter les requêtes JSON
 app.use(express.json());
+
 // Middleware pour parser les corps de requêtes
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Utilisation des routes pour l'authentification
-// Configuration des options cors
+
 // Middleware pour gérer les cors
+app.use(cors(corsOptions));
+
+// Configuration des options cors
+const corsOptions = {
+    credentials: true,
+    optionsSuccessStatus: 200,
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    preflightContinue: false,
+};
+
 // Définition du port demarrage du serveur
+const PORT = process.env.PORT || 5200;
+
 // Fonction pour demarrer le serveur
+const start = async () => {
+    try {
+        // Connexion a la base de donnée
+        // Démarrage du serveur sur le port spécifié
+        app.listen(PORT, () => console.log('Le serveur a démarré sur le port ${PORT}'));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 // Apple de la fonction pour demarrer le serveur
+start();
