91b46b428a3c1f05149a863fb7870e47b88002df
IgorChiousse <igor.chiousse@gmail.com>
2024-01-10 12:00:57 +0100

    création de la route protégée

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,29 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
+const authMiddleware = require('../middleware/authenticate');
 
 // Route pour l'inscription
 router.post('/register', authController.register);
 
 // Route pour la connection
 router.post('/login', authController.login);
+
+// Route protégée
+router.get('/dashboard', authMiddleware.authenticate, (req, res) => {
+	// Vérifier si l'utilisateur est un admin
+	if (req.user.role === 'admin') {
+		// Définition de req.isAdmin sera égale a true pour les admins
+		req.isAdmin = true;
+		// Envoyer un réponse de succès
+		return res.status(200).json({ message: 'Bienvenue Admin' });
+	} else {
+		// Envoyer une réponse pour les utilisateurs non admin
+		return res
+			.status(403)
+			.json({
+				message: 'action non autorisée, seul les admins peuvent accéder à cette page',
+			});
+	}
+});
 
 module.exports = router;
